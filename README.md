5eeda7443eddb78031adaecef7fe38649bd0737e
John Chou <luckyjoou@gmail.com>
2022-05-04 22:45:55 +0800

    Add bass drum at first up beat

--- README.md
+++ README.md
@@ -8,3 +8,15 @@
 #### Dependencies
 
 - miditoolkit@0.1.16
+
+### Instructions
+
+#### Arguments
+
+- `-i`: input midi file
+- `-o`: output midi file
+
+```python
+# Generate midi with comping tracks
+python main.py -i ./zh_pop/zhpop_117.mid -o output.mid
+```
--- main.py
+++ main.py
@@ -1,19 +1,64 @@
+import math
 import sys
 import getopt
 from miditoolkit import midi
 
+# Pitch of drum means tone in MuseScore
+# 36 - Bass
+
+# TODO:
+# - Time signature changes handler
+# - Expressions:
+#   - crescendo/diminuendo, fills/mute
+#   - Start to play with bass line
+def gen_drum_track(midi_obj):
+    beat_res = midi_obj.ticks_per_beat
+    print(beat_res)
+    # Program means tone?
+    track = midi.containers.Instrument(program=1, is_drum=True, name="Basic percussion")
+    for idx, tc in enumerate(midi_obj.tempo_changes):
+        tempo = round(tc.tempo)
+        # Tempo need to be in range [0, 127], why?
+        tempo = 127 if tempo > 127 else tempo
+        start_time = round(tc.time)
+        end_time = round(midi_obj.tempo_changes[idx + 1].time) if idx + 1 < len(midi_obj.tempo_changes) else midi_obj.max_tick
+        start = start_time
+        duration = round(beat_res * 0.5)
+        end = start + duration
+        beat_count = 1
+
+        while start < end_time:
+            if end >= end_time:
+                end = end_time
+            if beat_count == 4:
+                beat_count = 1
+            else:
+                if beat_count == 1:
+                    pitch = 36
+                    note = midi.containers.Note(start=start, end=end, velocity=tempo, pitch=pitch)
+                    track.notes.append(note)
+                beat_count += 1
+
+            start = start + beat_res
+            end = start + duration
+
+    # print(track.notes)
+    return track
 
 def digest_midi(midi_path, output_path):
     midi_obj = midi.parser.MidiFile(midi_path)
+
     lead_track = None
     for track in midi_obj.instruments:
         if track.name == "Lead":
             lead_track = track
-    print(midi_obj.instruments)
     assert lead_track != None
 
+    print(midi_obj.instruments)
+    drum_track = gen_drum_track(midi_obj)
+
     # Only keep lead track now
-    midi_obj.instruments = [lead_track]
+    midi_obj.instruments = [lead_track, drum_track]
     midi_obj.dump(output_path)
     print(f'Output file: {output_path}')
 
