05f287053808159e7a4d5391e63bfcc3744c8cfe
John Chou <luckyjoou@gmail.com>
2022-05-12 23:46:23 +0800

    Add basic piano track

---
+++ constants.py
@@ -0,0 +1,44 @@
+# normalize pitch if needed
+MIN_PITCH = 48
+MAX_PITCH = 72
+MIDDLE_C = 60
+
+NOTE_PITCHES = {
+    'Cb': MIDDLE_C - 1,
+    'C': MIDDLE_C,
+    'C#': MIDDLE_C + 1,
+    'Db': MIDDLE_C + 1,
+    'D': MIDDLE_C + 2,
+    'D#': MIDDLE_C + 3,
+    'Eb': MIDDLE_C + 3,
+    'E': MIDDLE_C + 4,
+    'E#': MIDDLE_C + 5,
+    'F': MIDDLE_C + 5,
+    'F#': MIDDLE_C + 6,
+    'Gb': MIDDLE_C + 6,
+    'G': MIDDLE_C + 7,
+    'G#': MIDDLE_C + 8,
+    'Ab': MIDDLE_C + 8,
+    'A': MIDDLE_C + 9,
+    'A#': MIDDLE_C + 10,
+    'Bb': MIDDLE_C + 10,
+    'B': MIDDLE_C + 11,
+    'B#': MIDDLE_C + 12
+}
+
+CHORD_TYPE_PITCHES = {
+    'm': [0, 3, 7],
+    'M': [0, 4, 7],
+    '': [0, 4, 7],
+    '+': [0, 4, 8],
+    'dim': [0, 3, 6],
+    '7': [0, 4, 7, 10],
+    'Mm7': [0, 4, 7, 10],
+    '%7': [0, 4, 7, 10],
+    'maj7': [0, 4, 7, 11],
+    'MM7': [0, 4, 7, 11],
+    'm7': [0, 3, 7, 10],
+    'mm7': [0, 3, 7, 10],
+    'm7b5': [0, 3, 6, 10],
+    'dim7': [0, 3, 6, 9]
+}
--- main.py
+++ main.py
@@ -1,20 +1,21 @@
+from logging import root
 import math
 import sys
 import getopt
+from matplotlib import markers
 from miditoolkit import midi
+from constants import NOTE_PITCHES, CHORD_TYPE_PITCHES
 
 # Pitch of drum means tone in MuseScore:
 #   Bass(36), Tom(38, 40), Clap(39), Snare(41), Hi-hat(42)
 
 # TODO:
-# - Time signature changes handler
 # - Expressions:
 #   - crescendo/diminuendo, fills/mute
 #   - Start to play with bass line
 # - 4/4 is too boring
 def gen_drum_track(midi_obj):
     beat_res = midi_obj.ticks_per_beat
-    print(beat_res)
     # Program means tone?
     track = midi.containers.Instrument(program=1, is_drum=True, name="Basic percussion")
     for idx, tc in enumerate(midi_obj.tempo_changes):
@@ -28,6 +29,7 @@ def gen_drum_track(midi_obj):
         end = start + duration
         beat_count = 1
 
+        # This way can't handle complicated rhythmic pattern
         while start < end_time:
             if end >= end_time:
                 end = end_time
@@ -48,7 +50,61 @@ def gen_drum_track(midi_obj):
             start = start + beat_res
             end = start + duration
 
-    # print(track.notes)
+    return track
+
+def get_piano_track(midi_obj):
+    beat_res = midi_obj.ticks_per_beat
+    track = midi.containers.Instrument(program=1, is_drum=False, name="Piano")
+
+    for idx, tc in enumerate(midi_obj.tempo_changes):
+        # TODO: duplicate from gen_drum_track, move it to a util func later
+        tempo = round(tc.tempo)
+        # Tempo need to be in range [0, 127], why?
+        tempo = 127 if tempo > 127 else tempo
+        tempo_start_time = round(tc.time)
+        tempo_end_time = round(midi_obj.tempo_changes[idx + 1].time) if idx + 1 < len(midi_obj.tempo_changes) else midi_obj.max_tick
+
+        for idx, marker in enumerate(midi_obj.markers):
+            chord_start_time = round(marker.time)
+            chord_end_time = round(midi_obj.markers[idx + 1].time) if idx + 1 < len(midi_obj.markers) else midi_obj.max_tick
+            if chord_start_time >= chord_end_time or chord_end_time < tempo_start_time or chord_start_time > tempo_end_time:
+                continue
+
+            # Current tempo in the period
+            chord_start_time = tempo_start_time if chord_start_time < tempo_start_time else chord_start_time
+            chord_end_time = tempo_end_time if tempo_end_time < chord_end_time else chord_end_time
+
+            [root_note, chord_type] = marker.text.split('_')
+            if root_note is None or chord_type is None or root_note not in NOTE_PITCHES or chord_type not in CHORD_TYPE_PITCHES:
+                print(f'Unrecognized note {root_note} or chord type {chord_type}')
+                continue
+
+            base_pitch = NOTE_PITCHES[root_note]
+            chord_pitches = CHORD_TYPE_PITCHES[chord_type]
+            chord_note_num = len(chord_pitches)
+
+            start = chord_start_time
+            duration = round(beat_res)
+            end = start + duration
+            chord_note_idx = 0
+            step = 1
+
+            while start < chord_end_time:
+                if end >= chord_end_time:
+                    end = chord_end_time
+                if chord_note_idx == chord_note_num - 1:
+                    step = -1
+                elif chord_note_idx == -1:
+                    chord_note_idx = 1
+                    step = 1
+
+                pitch = base_pitch + chord_pitches[chord_note_idx]
+                note = midi.containers.Note(start=start, end=end, velocity=tempo, pitch=pitch)
+                track.notes.append(note)
+                start = start + beat_res
+                end = start + duration
+                chord_note_idx += step
+
     return track
 
 def digest_midi(midi_path, output_path):
@@ -61,10 +117,12 @@ def digest_midi(midi_path, output_path):
     assert lead_track != None
 
     print(midi_obj.instruments)
+
     drum_track = gen_drum_track(midi_obj)
+    piano_track = get_piano_track(midi_obj)
 
     # Only keep lead track now
-    midi_obj.instruments = [lead_track, drum_track]
+    midi_obj.instruments = [lead_track, drum_track, piano_track]
     midi_obj.dump(output_path)
     print(f'Output file: {output_path}')
 
